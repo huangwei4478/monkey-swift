6cf43aad01f2c0430bb58a6dad0c88257fb44dfb
huangwei <harw.op@qq.com>
2021-11-27 13:56:48 +0800

    [fix] 参照 hermes的测试搞定了testcase，但我还是没搞懂到底一整个发生了什么

--- MonkeySwiftUnitTest/CodeTest.swift
+++ MonkeySwiftUnitTest/CodeTest.swift
@@ -10,49 +10,18 @@ import XCTest
 
 class CodeTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+	typealias Test = (opCode: OpCodes, operands: [Int32], expected: [Byte])
+	
 	func testMake() {
-		struct TestCase {
-			let op: Opcode
-			let operands: [Int]
-			let expected: [Byte]
-			
-			init(_ op: Opcode, _ operands: [Int], _ expected: [Byte]) {
-				self.op = op
-				self.operands = operands
-				self.expected = expected
-			}
-		}
-		
-		let testCases: [TestCase] = [
-			TestCase(OpcodeEnum.OpConstant.rawValue, [65534],
-					 [Byte(OpcodeEnum.OpConstant.rawValue),
-					  Byte(bitPattern: 255),
-					  Byte(bitPattern: 254)
-					 ]
-					)
+		let tests: [Test] = [
+			(OpCodes.constant, [65534], [OpCodes.constant.rawValue, 255, 254])
 		]
 		
-		for testCase in testCases {
-			let instruction = make(op: testCase.op, operands: testCase.operands)
-			
-			guard instruction.count == testCase.expected.count else {
-				XCTFail("instruction has wrong length. want=\(testCase.expected.count), got=\(instruction.count)")
-				return
-			}
-			
-			for (index, byte) in testCase.expected.enumerated() {
-				guard instruction[index] == byte else {
-					XCTFail("wrong byte at pos \(index). want=\(byte), got=\(instruction[index])")
-					return
-				}
+		for test in tests {
+			let instructions = Bytecode.make(test.opCode, operands: test.operands)
+			XCTAssertEqual(instructions.count, test.expected.count)
+			for index in 0 ..< test.expected.count {
+				XCTAssertEqual(instructions[index], test.expected[index])
 			}
 		}
 	}
